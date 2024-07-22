b260b0521a23be162bca7d6b1fadefbcba93a1c4
Mykhailo Lipilov <m.lipilov@andersenlab.com>
2024-07-22 15:42:15 +0300

    123

--- src/main/java/com/andersen/userservice/scheduler/UserCsvTransferJobScheduler.java
+++ src/main/java/com/andersen/userservice/scheduler/UserCsvTransferJobScheduler.java
@@ -2,10 +2,13 @@ package com.andersen.userservice.scheduler;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -32,10 +35,10 @@ public class UserCsvTransferJobScheduler {
   @SneakyThrows
   public void scheduleUserCsvTransferJob() {
     log.info("Scheduling user csv transfer job");
-//    final JobParameters params = new JobParametersBuilder()
-//        .addString(JOB_NAME, UUID.randomUUID().toString())
-//        .toJobParameters();
-//    jobLauncher.run(myBatchJob, params);
+    final JobParameters params = new JobParametersBuilder()
+        .addString(JOB_NAME, UUID.randomUUID().toString())
+        .toJobParameters();
+    jobLauncher.run(myBatchJob, params);
     log.info("Job completed successfully");
   }
 }
